453806ce1dd2b04b8f1234589644898688cc8597
neosyk-net <runerdycastillo@gmail.com>
2025-09-24 18:01:11 -0400

    email form

---
+++ index.js
@@ -0,0 +1,27 @@
+// template_s3chdi9
+// service_006c53z
+// 5FCkLIgTwdMXbV0pi
+// <script type="text/javascript"
+        
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_006c53z',
+            'template_s3chdi9',
+            event.target,
+            '5FCkLIgTwdMXbV0pi'
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly at runerdy@gmail.com"
+            );
+        })
+    }
+
